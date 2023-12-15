d95fe89f33024c05590f583e3e949b1eb619f9f9
fx67ll <2717745014@qq.com>
2023-12-15 09:56:54 +0800

    fix script

--- js/baoiduCount.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-	var hm = document.createElement("script");
-	hm.src = "https://hm.baidu.com/hm.js?a5ed5564cbfffe6ea12d12bf87fbefc4";
-	var s = document.getElementsByTagName("script")[0];
-	s.parentNode.insertBefore(hm, s);
-})();
